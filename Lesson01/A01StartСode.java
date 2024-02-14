f9d32552f5ca4f2416df0bb47ec1c0ecb104a128
Vit <mayllby@gmail.com>
2024-02-14 14:42:33 +0300

    Добавил 'A01StartСode.java'

---
+++ Lesson01/A01StartСode.java
@@ -0,0 +1,55 @@
+// Java: знакомство и как пользоваться базовым API (лекции)
+// Урок 1. Знакомство с языком программирования Java
+// https://gb.ru/lessons/404312
+
+package JavaCourse.Lesson01;
+/** 
+* Program
+* многострочные комментарии слеш + звёздочка
+*/ 
+public class A01StartСode {
+    public static void main(String[] args) {
+        System.out.println("world");  // классический комментарий (два слеша)
+                                        // базовый класс "main",
+                                        // "String[]" принимает массив строк, 
+                                        // в котором и пишем код
+        System.out.println("Goodbye world");
+    } 
+}
+
+
+/*
+Почему JAVA
+
+1. Топ 3 ЯП в мире
+2. Библиотеки
+3. Кроссплатформенность
+4. Тысячи вакансий разных направлений
+5. Начало Android’а (упомянут ещё какой-то один язык)
+6. Безопасность
+7. ООП
+8. Многопоточность, как она есть
+
+00:06:25
+
+Настройка рабочего места
+
+1. Основы основ
+   Типы, базовые конструкции
+2. Детальный разбор некоторых API
+   Файлы и работа с потоками ввода\вывода
+3. Погружение в Java Collection API
+   List’ы и не только
+
+
+Настройка рабочего места
+
+Шаг 1: 
+Java JDK https://www.oracle.com/java/technologies/downloads/
+Шаг 2: 
+Extension Pack VS Code
+https://marketplace.visualstudio.com/items?itemName=vscjava.vscode-javapack
+При желаниии: 
+https://www.jetbrains.com/ru-ru/idea/
+println(“Hello world”);
+*/
