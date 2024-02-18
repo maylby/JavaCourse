b21681a06845c829c0f7554f1af1ec296d901ae7
Vit <mayllby@gmail.com>
2024-02-18 21:25:20 +0300

    Добавил заголовок

---
+++ Lesson02/L05ByteFiles.java
@@ -0,0 +1,17 @@
+/*
+Урок 2. Почему вы не можете не использовать API
+https://gb.ru/lessons/404313
+
+
+00:17:00
+
+Бинарные файлы
+(см. код в доп. материалах к уроку)
+ */
+
+package JavaCourse.Lesson02;
+
+
+public class L05ByteFiles {
+    
+}
