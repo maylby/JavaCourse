58642f433dd7319f2fd5f10ae949b106b71d1ac7
Vit <mayllby@gmail.com>
2024-02-18 17:07:22 +0300

    Добавил код

--- Lesson02/L04Functional.java
+++ Lesson02/L04Functional.java
@@ -33,6 +33,20 @@ renameTo(File dest): переименовывает файл или катало
  */
 package JavaCourse.Lesson02;
 
+import java.io.File;
+
 public class L04Functional {
-    
+    public static void main(String[] args) {
+ 		String pathProject = System.getProperty("user.dir");
+ 		String pathDir = pathProject.concat("/files");
+ 		File dir = new File(pathDir);
+ 		System.out.println(dir.getAbsolutePath ());
+ 		if (dir.mkdir()) { // создает новый каталог
+ 			System.out.println("+");
+ 		} else {
+ 			System.out.println("-");
+ 		}
+ 		for (String fname : dir.list()) {
+ 			System.out.println(fname);
+ 		}}
 }
