8b01c3f36176efee9fc39a2bc67b9a8ded48078f
Vit <mayllby@gmail.com>
2024-02-18 12:48:29 +0300

    Добавил заголовок

---
+++ Lesson02/L01String.java
@@ -0,0 +1,13 @@
+/*
+Java: знакомство и как пользоваться базовым API (лекции)
+Урок 2. Почему вы не можете не использовать API
+https://gb.ru/lessons/404313
+
+
+
+ */
+package JavaCourse.Lesson02;
+
+public class L01String {
+    
+}
