f43d6407387ed87b096f5dcdcf2d231309ffa886
Vit <mayllby@gmail.com>
2024-02-18 14:16:24 +0300

    Добавил заголовок

---
+++ Lesson02/L02Files.java
@@ -0,0 +1,14 @@
+/*
+Урок 2. Почему вы не можете не использовать API
+https://gb.ru/lessons/404313
+
+
+00:08:00
+
+Работа с файловой системой. Файлы
+ */
+package JavaCourse.Lesson02;
+
+public class L02Files {
+    
+}
