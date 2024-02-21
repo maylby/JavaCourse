d8d61606104f296e48cdcc5a1a8ffa8906878d14
Vit <mayllby@gmail.com>
2024-02-21 18:25:30 +0300

    Добавил заголовок

---
+++ Lesson05/L01HashMap.java
@@ -0,0 +1,11 @@
+/*
+Java: знакомство и как пользоваться базовым API (лекции)
+Урок 5. Хранение и обработка данных ч2: множество коллекций Map
+https://gb.ru/lessons/404316
+
+ */
+package JavaCourse.Lesson05;
+
+public class L01HashMap {
+    
+}
