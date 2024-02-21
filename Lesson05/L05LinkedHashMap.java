613ae77300b3bb23a3501e5afcd44c45f7281321
Vit <mayllby@gmail.com>
2024-02-22 00:47:07 +0300

    Добавил заголовок

---
+++ Lesson05/L05LinkedHashMap.java
@@ -0,0 +1,18 @@
+/*
+Урок 5. Хранение и обработка данных ч2: множество коллекций Map
+https://gb.ru/lessons/404316
+
+00:32:00
+
+LinkedHashMap
+
+“Старший брат” коллекции HashMap, который всё помнит…
+Помнит порядок добавления элементов ➜ более медлительный
+Map<Integer,String> linkmap = new LinkedHashMap<>();
+
+ */
+package JavaCourse.Lesson05;
+
+public class L05LinkedHashMap {
+    
+}
