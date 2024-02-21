f651ffab264c864093422b04cbe5400160b2c7ff
Vit <mayllby@gmail.com>
2024-02-22 00:58:37 +0300

    Добавил заголовок

---
+++ Lesson05/L06HashTable.java
@@ -0,0 +1,16 @@
+/*
+Урок 5. Хранение и обработка данных ч2: множество коллекций Map
+https://gb.ru/lessons/404316
+
+
+HashTable
+
+«Устаревший брат» коллекции HashMap,
+который не знает про null
+
+ */
+package JavaCourse.Lesson05;
+
+public class L06HashTable {
+    
+}
