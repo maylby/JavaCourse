61783f63724120ffba559074e1f94c6c2907e026
Vit <mayllby@gmail.com>
2024-02-22 00:59:04 +0300

    Добавил код

--- Lesson05/L06HashTable.java
+++ Lesson05/L06HashTable.java
@@ -11,6 +11,16 @@ HashTable
  */
 package JavaCourse.Lesson05;
 
+import java.util.Hashtable;
+import java.util.Map;
+
 public class L06HashTable {
-    
+        public static void main(String[] args) {
+        Map<Integer,String> table = new Hashtable<>();
+        table.put(1, "два");
+        table.put(11, "один один");
+        table.put(2, "один");
+        System.out.println(table); // {2=один, 1=два, 11=один один}
+        // table.put(null, "один"); //  java.lang.NullPointerException
+    }
 }
