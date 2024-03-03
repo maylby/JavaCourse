ef27e18d6dd55d29317aea1d896e5496b4826742
Vit <mayllby@gmail.com>
2024-03-03 17:35:07 +0300

    Добавил пояснения

--- Lesson06/L01Info.java
+++ Lesson06/L01Info.java
@@ -113,19 +113,29 @@ HashSet как синоним множества
 
     /*
      * Код - Доп.мат. (Part1)
-     * или презентация (стр.11)
+     * или презентация (стр. 11, 12)
      */
 class Ex002_MathSet {
     public static void main(String[] args) {
+        /*
+         * переменной (a) типа (var) передать 
+         * определённую коллекцию (HashSet), для которой
+         * заданы значения в неком массиве (asList)
+         */
         var a = new HashSet<>(Arrays.asList(1,2,3,4,5,6,7));
         var b = new HashSet<>(Arrays.asList(2,3,5,7,11,13,17));
-        var u = new HashSet<Integer>(a); u.addAll(b);
-        var r = new HashSet<Integer>(a); r.retainAll(b);
-        var s = new HashSet<Integer>(a); s.removeAll(b);
+        var u = new HashSet<Integer>(a); u.addAll(b); // "u" - добавление только уникальных значений
+                                                      // (объединение множеств)
+        var r = new HashSet<Integer>(a); r.retainAll(b); // "r" - выбор общих значений коллекций
+                                                         // (пересечение множеств)
+        var s = new HashSet<Integer>(a); s.removeAll(b); // удаление уникальных значений,
+                                                         // в заданном промежутке (?)
         System.out.println(a); // [1, 2, 3, 4, 5, 6]
         System.out.println(b); // [17, 2, 3, 5, 7, 11]
         System.out.println(u); // [1, 17, 2, 3, 4, 5, 6, 11]
         System.out.println(r); // [2, 3, 5, 7]
         System.out.println(s); // [1, 4, 6]
+        boolean res = a.addAll(b);
+        System.out.println(res);
     }
 }
