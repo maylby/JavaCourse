df5ff8fff08820535cc8060b736dbcd6e681ef69
Vit <mayllby@gmail.com>
2024-03-03 16:08:51 +0300

    Добавил код 'HashSet'

--- Lesson06/L01Info.java
+++ Lesson06/L01Info.java
@@ -60,4 +60,37 @@ size() – возвращает количество элементов колл
 HashSet (код)
 
  */
- 
+package JavaCourse.Lesson06;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class L01Info {
+    
+}
+/*
+ * Код из презентации (стр.9)
+ * или Доп.мат. (Part1)
+ */
+class Ex001_HashSet {
+    public static void main(String[] args) {
+    Set<Integer> set = new HashSet<>();
+    set.add(1); set.add(12); set.add(123);
+    set.add(1234); set.add(1234);
+    System.out.println(set.contains(12)); // true
+    set.add(null);
+    System.out.println(set.size()); // 5
+    System.out.println(set); // [null, 1, 1234, 123, 12]
+    set.remove(12);
+    /*
+     * "println" в коде из презентации 
+     * записан в одну строку с циклом (for), 
+     * без фигурных скобок "{}" (см. ниже),
+     * в дополнительных материалах к лекции 
+     * форма записи кода стандартная, со скобками
+     */
+    for (var item : set) System.out.println(item); // null 1 1234 123
+    set.clear();
+    System.out.println(set); // []
+    }
+}  
