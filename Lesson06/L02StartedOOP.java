1c38724490cc507afeda946ff30c1fa1b6b09dc7
Vit <mayllby@gmail.com>
2024-03-03 21:25:27 +0300

    Добавил правки

--- Lesson06/L02StartedOOP.java
+++ Lesson06/L02StartedOOP.java
@@ -136,7 +136,7 @@ class Worker {
          * форма записи в одну строку
          * нет переменной (result), которой пеередаётся строка "format",
          * вместо передачи переменной "result" функции возврат (return),
-         * передаётся, непосредственно, искомое значение (строка)
+         * передаётся, непосредственно, строка "format"
          */
         return String.format("id: %d fn: %s ln: %s s: %d", id, firstName, lastName, salary);
     }
@@ -146,7 +146,7 @@ class Worker {
         var t = (Worker) o;
         return id == t.id && firstName == t.firstName;
     }
-    
+
     @Override
     public int hashCode() {
         return id;
