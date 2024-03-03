d58236e71d4971d3859826abf6b2e91d3cf26b62
Vit <mayllby@gmail.com>
2024-03-03 15:29:20 +0300

    Добавил план

---
+++ Lesson06/L01Info.java
@@ -0,0 +1,19 @@
+/*
+Java: знакомство и как пользоваться базовым API (лекции)
+Урок 6. Хранение и обработка данных ч3: множество коллекций Set
+https://gb.ru/lessons/404317
+
+Пдан урока:
+
+- Обзор функционала
+- Set
+- HashSet
+- TreeSet
+- LinkedHashSet
+
+ */
+package JavaCourse.Lesson06;
+
+public class L01Info {
+    
+}
